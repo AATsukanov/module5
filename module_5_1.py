5ff7a8fab1fbe73221c7c1a9889b44a234182b11
Alexey A. Tsukanov <a.a.tsukanov@yandex.ru>
2024-06-30 13:43:57 +0300

    Добавлено решение в module_5_1 к задаче №2

--- module_5_1.py
+++ module_5_1.py
@@ -73,10 +73,11 @@ class House:
         return self.nFloors
 
     def __str__(self):
-        return f'Название: {self.name}, кол-во этажей: {self.nFloors}.'
+        return f'Название: "{self.name}", кол-во этажей: {self.nFloors}.'
 
 def main():
     # №1
+    print('ДЗ-1:')
     h1 = House('ЖК Горский', 18)
     h2 = House('Домик в деревне', 2)
     h3 = House('ЖК Летний', 22)
@@ -84,7 +85,7 @@ def main():
     h2.go_to(10)
     h3.go_to(-2)
     # №2
-    print(f'ДЗ-2: {h1}, {h2}, {h3}')
+    print(f'ДЗ-2:\n{h1}\n{h2}\n{h3}')
     print(len(h1), len(h2), len(h3))
 
 if __name__ == '__main__':
